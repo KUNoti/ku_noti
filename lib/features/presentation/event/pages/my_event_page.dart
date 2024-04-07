c3cbff5b0c230a658bf4758861de44bd7b7e9169
WhatWR <kul.b@ku.th>
2024-04-07 17:07:10 +0700

    follow tag

--- lib/features/presentation/event/pages/my_event_page.dart
+++ lib/features/presentation/event/pages/my_event_page.dart
@@ -2,10 +2,17 @@
 import 'package:flutter/material.dart';
 import 'package:ku_noti/core/constants/colors.dart';
 import 'package:ku_noti/features/presentation/event/widgets/create_event_dialog.dart';
+import 'package:ku_noti/features/presentation/event/widgets/select_tag_follow.dart';
 
-class MyEventPage extends StatelessWidget {
+class MyEventPage extends StatefulWidget {
   const MyEventPage({super.key});
 
+  @override
+  State<MyEventPage> createState() => _MyEventPageState();
+}
+
+class _MyEventPageState extends State<MyEventPage> {
+  Set<int> selectedTagIndices = {};
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,11 +22,6 @@ class MyEventPage extends StatelessWidget {
         )
     );
   }
-
-  // void _handleCreateEvent(BuildContext context) {
-  //
-  // }
-
   Future openDialog(BuildContext context) => showDialog(
     context: context,
     builder: (context) => const CreateEventDialog(),
@@ -60,16 +62,36 @@ class MyEventPage extends StatelessWidget {
   }
 
   Widget _buildBody(BuildContext context) {
-    return Column(
-      children: [
-
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          _buildFollowTagSection(context),
+        ],
+      ),
     );
   }
 
   Widget _buildFollowTagSection(BuildContext context) {
     return Column(
-      children: [],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("Follow Tag", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        SelectTagFollow(
+          selectedChipIndices: selectedTagIndices,
+          onChipSelected: (String label, bool isSelected, int index) {
+            setState(() {
+              if (isSelected) {
+                selectedTagIndices.add(index);
+                // print("Selected: $label");
+              } else {
+                selectedTagIndices.remove(index);
+                // print("Deselected: $label");
+              }
+            });
+          },
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/presentation/event/widgets/select_tag_follow.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/material.dart';
+import 'package:ku_noti/core/constants/colors.dart';
+
+typedef ChipSelectedCallback = void Function(String selectedLabel, bool isSelected, int index);
+
+class SelectTagFollow extends StatelessWidget {
+  final List<String> chipLabels = ['KU', 'Music', 'Art', 'Workshop'];
+  final List<IconData> chipIcons = [Icons.school, Icons.music_note, Icons.brush, Icons.build];
+  final ChipSelectedCallback onChipSelected;
+  final Set<int> selectedChipIndices;
+
+  SelectTagFollow({
+    super.key,
+    required this.selectedChipIndices,
+    required this.onChipSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: chipLabels.length,
+        itemBuilder: (context, index) {
+          bool isSelected = selectedChipIndices.contains(index);
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: ChoiceChip(
+              showCheckmark: false,
+              avatar: Icon(
+                chipIcons[index],
+                color: isSelected ? Colors.white : MyColors().primary,
+              ),
+              label: Text(
+                chipLabels[index],
+                style: TextStyle(
+                    color: isSelected ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              backgroundColor: isSelected ? MyColors().primary : Colors.white10,
+              selectedColor: MyColors().primary,
+              selected: isSelected,
+              onSelected: (bool selected) {
+                Set<int> newSelectedIndices = Set.from(selectedChipIndices);
+                if (selected) {
+                  newSelectedIndices.add(index);
+                } else {
+                  newSelectedIndices.remove(index);
+                }
+                // Call the callback with only the label of the changed tag
+                onChipSelected(chipLabels[index], selected, index);
+              },
+              shape: StadiumBorder(
+                side: BorderSide(color: MyColors().primary),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
